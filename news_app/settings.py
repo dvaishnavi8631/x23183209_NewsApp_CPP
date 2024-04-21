93899d471bd0cd230891323b5bdab058c95ff61e
EC2 Default User <ec2-user@ip-172-31-17-27.eu-west-1.compute.internal>
2024-04-21 23:26:43 +0000

    updating the env

--- news_app/settings.py
+++ news_app/settings.py
@@ -149,12 +149,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # AWS_ACCESS_KEY_ID = 'your-access-key-id'
 # AWS_SECRET_ACCESS_KEY = 'your-secret-access-key'
-AWS_STORAGE_BUCKET_NAME = 'x23183209-cpp-newsbite'
-AWS_S3_REGION_NAME = 'eu-north-1'  # e.g., 'us-east-1'
+# AWS_STORAGE_BUCKET_NAME = 'x23183209-cpp-newsbite'
+# AWS_S3_REGION_NAME = 'eu-north-1'  # e.g., 'us-east-1'
 # STATIC_URL = 'https://%s.s3.amazonaws.com/static/' % AWS_STORAGE_BUCKET_NAME
 
 # Tell Django to use S3 to store static files.
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # # Tell Django to use S3 to store uploaded media files.
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
