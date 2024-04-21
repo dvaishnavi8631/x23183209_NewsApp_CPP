623bd2e328d63f68975e66dc8179ec41e4b32fed
EC2 Default User <ec2-user@ip-172-31-17-27.eu-west-1.compute.internal>
2024-04-21 22:44:38 +0000

    creating env

--- news_app/asgi.py
+++ news_app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Newsapp project.
+ASGI config for news_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Newsapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news_app.settings')
 
 application = get_asgi_application()
