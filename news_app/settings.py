282820683254f2e2354fad5bec23cd7cf4d93d37
EC2 Default User <ec2-user@ip-172-31-17-27.eu-west-1.compute.internal>
2024-04-23 11:07:06 +0000

    updating the static

--- news_app/settings.py
+++ news_app/settings.py
@@ -144,15 +144,3 @@ CRISPY_TEMPLATE_PACK="bootstrap4"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# settings.py
-# AWS_ACCESS_KEY_ID = 'your-access-key-id'
-# AWS_SECRET_ACCESS_KEY = 'your-secret-access-key'
-# AWS_STORAGE_BUCKET_NAME = 'x23183209-cpp-newsbite'
-
-# # For media files
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# MEDIA_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-
-# # For static files
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# STATIC_URL = 'https://%s.s3.amazonaws.com/static/' % AWS_STORAGE_BUCKET_NAME
